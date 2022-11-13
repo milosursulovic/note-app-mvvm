b2caeacbc9969fa2edca457510a52cea87eeb347
Misa <milosursulovic2696@gmail.com>
2022-11-14 00:13:36 +0100

    Add delete note use case and notes view model

--- app/src/main/java/com/example/noteapp/di/AppModule.kt
+++ app/src/main/java/com/example/noteapp/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.example.noteapp.feature_note.data.data_source.NoteDatabase
 import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.example.noteapp.feature_note.domain.repository.NoteRepository
+import com.example.noteapp.feature_note.domain.use_case.AddNote
 import com.example.noteapp.feature_note.domain.use_case.DeleteNote
 import com.example.noteapp.feature_note.domain.use_case.GetNotes
 import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
@@ -35,6 +36,7 @@ object AppModule {
     @Singleton
     fun providesNoteUseCases(repository: NoteRepository): NoteUseCases = NoteUseCases(
         getNotes = GetNotes(repository),
-        deleteNote = DeleteNote(repository)
+        deleteNote = DeleteNote(repository),
+        addNote = AddNote(repository)
     )
 }
